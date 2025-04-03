801c4e176e4a0588371d29da38ddc337ef6e05f6
Shaun Chong <data-miner00@users.noreply.github.com>
2025-04-03 20:34:33 +0800

    feat: Add user endpoints

---
+++ Vitals.WebApi/Controllers/UserController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Vitals.WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly UserRepository repository;
+
+        public UserController(UserRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetById(int id)
+        {
+            try
+            {
+                return Ok(this.repository.Get(id));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(this.repository.GetAll());
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] User user)
+        {
+            try
+            {
+                this.repository.Add(user);
+                return Created();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut("{id:int}")]
+        public IActionResult Update(int id, [FromBody] User user)
+        {
+            try
+            {
+                this.repository.Update(user);
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ Vitals.WebApi/UserRepository.cs
@@ -0,0 +1,42 @@
+﻿namespace Vitals.WebApi
+{
+    public class UserRepository
+    {
+        private List<User> users = new List<User>();
+
+        public UserRepository()
+        {
+            this.users.Add(new User
+            {
+                Id = 1,
+                Email = "test@tesma.com",
+                Name = "Test",
+            });
+        }
+
+        public List<User> GetAll()
+        {
+            return users;
+        }
+
+        public void Add(User user)
+        {
+            users.Add(user);
+        }
+
+        public User Get(int id)
+        {
+            return this.users.First(x => x.Id == id);
+        }
+
+        public void Update(User user)
+        {
+            users = [..users.Where(x => x.Id != user.Id), user];
+        }
+
+        public void Delete(int id)
+        {
+            users = users.Where(x => x.Id != id).ToList();
+        }
+    }
+}
