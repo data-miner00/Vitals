298071b216538c0d8daa7b3eeb51d5d0c3272094
Shaun Chong <data-miner00@users.noreply.github.com>
2025-04-30 21:26:30 +0800

    feat: Setup otel

--- Vitals.WebApi/Controllers/UserController.cs
+++ Vitals.WebApi/Controllers/UserController.cs
@@ -1,28 +1,44 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Diagnostics.Metrics;
 
 namespace Vitals.WebApi.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
+    [Area("users")]
     public class UserController : ControllerBase
     {
+        private readonly Counter<int> counter;
+        private readonly ActivitySource source;
         private readonly UserRepository repository;
 
-        public UserController(UserRepository repository)
+        public UserController(UserRepository repository, Meter greeterMeter, ActivitySource source)
         {
             this.repository = repository;
+            this.counter = greeterMeter.CreateCounter<int>("Counter");
+            this.source = source;
         }
 
         [HttpGet("{id:int}")]
         public IActionResult GetById(int id)
         {
+            using var activity = this.source.StartActivity("GetById", ActivityKind.Server);
             try
             {
+                this.counter.Add(1);
                 return Ok(this.repository.Get(id));
             }
             catch (Exception ex)
             {
                 return BadRequest(ex.Message);
+            }
+            finally
+            {
+                activity?.SetTag("id", id);
+                activity?.SetTag("controller", nameof(UserController));
+                activity?.SetTag("action", nameof(GetById));
+                activity?.SetTag("area", "users");
             }
         }
 
--- Vitals.WebApi/Program.cs
+++ Vitals.WebApi/Program.cs
@@ -1,5 +1,10 @@
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using System.Diagnostics;
+using System.Diagnostics.Metrics;
 using Vitals.WebApi;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,6 +18,52 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddHealthChecks();
+
+using var greeterMeter = new Meter("Vitals.WebApi", "1.0.0");
+
+builder.Services.AddSingleton(greeterMeter);
+
+var tracingOtlpEndpoint = builder.Configuration["OTLP_ENDPOINT_URL"];
+var otel = builder.Services.AddOpenTelemetry();
+
+// Configure OpenTelemetry Resources with the application name
+otel.ConfigureResource(resource => resource
+    .AddService(serviceName: builder.Environment.ApplicationName));
+
+// Add Metrics for ASP.NET Core and our custom metrics and export to Prometheus
+otel.WithMetrics(metrics => metrics
+    // Metrics provider from OpenTelemetry
+    .AddAspNetCoreInstrumentation()
+    .AddMeter(greeterMeter.Name)
+    // Metrics provides by ASP.NET Core in .NET 8
+    .AddMeter("Microsoft.AspNetCore.Hosting")
+    .AddMeter("Microsoft.AspNetCore.Server.Kestrel")
+    // Metrics provided by System.Net libraries
+    .AddMeter("System.Net.Http")
+    .AddMeter("System.Net.NameResolution")
+    .AddPrometheusExporter());
+
+var activitySource = new ActivitySource("Vitals.WebApi");
+builder.Services.AddSingleton(activitySource);
+
+// Add Tracing for ASP.NET Core and our custom ActivitySource and export to Jaeger
+otel.WithTracing(tracing =>
+{
+    tracing.AddAspNetCoreInstrumentation();
+    tracing.AddHttpClientInstrumentation();
+    tracing.AddSource(activitySource.Name);
+    if (tracingOtlpEndpoint != null)
+    {
+        tracing.AddOtlpExporter(otlpOptions =>
+        {
+            otlpOptions.Endpoint = new Uri(tracingOtlpEndpoint);
+        });
+    }
+    else
+    {
+        tracing.AddConsoleExporter();
+    }
+});
 
 var app = builder.Build();
 
@@ -39,5 +90,7 @@ app.MapHealthChecks("/health", new HealthCheckOptions
         [HealthStatus.Unhealthy] = StatusCodes.Status503ServiceUnavailable,
     },
 });
+
+app.MapPrometheusScrapingEndpoint();
 
 app.Run();
