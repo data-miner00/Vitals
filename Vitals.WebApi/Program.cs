51f3927f4e019dccca77327a7ece47c129b4e6a4
Shaun Chong <data-miner00@users.noreply.github.com>
2025-05-21 08:13:14 +0800

    refactor: Organize relavant blocks

--- Vitals.WebApi/Program.cs
+++ Vitals.WebApi/Program.cs
@@ -2,6 +2,7 @@ namespace Vitals.WebApi;
 
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
+using OpenTelemetry;
 using OpenTelemetry.Logs;
 using OpenTelemetry.Metrics;
 using OpenTelemetry.Resources;
@@ -20,28 +21,9 @@ public static class Program
         builder.Services.AddControllers();
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
-
         builder.Services.AddHealthChecks();
 
-        builder.Logging
-            .ClearProviders()
-            .AddConsole()
-            .AddDebug()
-            .AddOpenTelemetry(opt =>
-            {
-                opt.AddOtlpExporter(options =>
-                {
-                    options.Endpoint = new Uri(builder.Configuration["OTLP_ENDPOINT_URL"]);
-                });
-
-                var resourceBuilder = ResourceBuilder.CreateDefault()
-                    .AddService(builder.Environment.ApplicationName);
-
-                opt.AddConsoleExporter()
-                    .SetResourceBuilder(resourceBuilder);
-
-                opt.IncludeScopes = true;
-            });
+        builder.RegisterLoggings();
 
         using var greeterMeter = new Meter("Vitals.WebApi", "1.0.0");
         builder.Services.AddSingleton(greeterMeter);
@@ -49,58 +31,15 @@ public static class Program
         var otlpEndpoint = builder.Configuration["OTLP_ENDPOINT_URL"];
         var otel = builder.Services.AddOpenTelemetry();
 
-        // Configure OpenTelemetry Resources with the application name
         otel.ConfigureResource(resource => resource
             .AddService(serviceName: builder.Environment.ApplicationName));
 
-        // Add Metrics for ASP.NET Core and our custom metrics and export to Prometheus
-        otel.WithMetrics(metrics =>
-        {
-            metrics
-                // Metrics provider from OpenTelemetry
-                .AddAspNetCoreInstrumentation()
-                .AddRuntimeInstrumentation()
-                .AddProcessInstrumentation()
-                .AddMeter(greeterMeter.Name)
-                // Metrics provides by ASP.NET Core in .NET 8
-                .AddMeter("Microsoft.AspNetCore.Hosting")
-                .AddMeter("Microsoft.AspNetCore.Server.Kestrel")
-                // Metrics provided by System.Net libraries
-                .AddMeter("System.Net.Http")
-                .AddMeter("System.Net.NameResolution")
-                .AddOtlpExporter(options =>
-                {
-                    options.Endpoint = new Uri(otlpEndpoint);
-                })
-                .AddPrometheusExporter();
-        });
+        otel.AddMetrics(meterName: builder.Environment.ApplicationName, otlpEndpoint);
 
         var activitySource = new ActivitySource("Vitals.WebApi");
         builder.Services.AddSingleton(activitySource);
 
-        // Add Tracing for ASP.NET Core and our custom ActivitySource and export to Jaeger
-        otel.WithTracing(tracing =>
-        {
-            tracing.AddSource(builder.Environment.ApplicationName)
-                .SetResourceBuilder(ResourceBuilder.CreateDefault()
-                    .AddService(builder.Environment.ApplicationName));
-            tracing.AddAspNetCoreInstrumentation();
-            tracing.AddHttpClientInstrumentation();
-            tracing.AddSource(activitySource.Name);
-            if (otlpEndpoint != null)
-            {
-                tracing.AddOtlpExporter(otlpOptions =>
-                {
-                    otlpOptions.Endpoint = new Uri(otlpEndpoint);
-                });
-
-                tracing.AddJaegerExporter(); // Remove this for OTLP
-            }
-            else
-            {
-                tracing.AddConsoleExporter();
-            }
-        });
+        otel.AddTracing(builder.Environment.ApplicationName, activitySource.Name, otlpEndpoint);
 
         var app = builder.Build();
 
@@ -126,5 +65,93 @@ public static class Program
                 [HealthStatus.Unhealthy] = StatusCodes.Status503ServiceUnavailable,
             },
         });
+    }
+
+    private static WebApplicationBuilder RegisterLoggings(this WebApplicationBuilder builder)
+    {
+        builder.Logging
+            .ClearProviders()
+            .AddConsole()
+            .AddDebug()
+            .AddOpenTelemetry(opt =>
+            {
+                opt.AddOtlpExporter(options =>
+                {
+                    options.Endpoint = new Uri(builder.Configuration["OTLP_ENDPOINT_URL"]);
+                });
+
+                var resourceBuilder = ResourceBuilder.CreateDefault()
+                    .AddService(builder.Environment.ApplicationName);
+
+                opt.AddConsoleExporter()
+                    .SetResourceBuilder(resourceBuilder);
+
+                opt.IncludeScopes = true;
+            });
+
+        return builder;
+    }
+
+    private static OpenTelemetryBuilder AddMetrics(
+        this OpenTelemetryBuilder otel,
+        string meterName,
+        string otlpEndpoint)
+    {
+        // Add Metrics for ASP.NET Core and our custom metrics and export to Prometheus
+        otel.WithMetrics(metrics =>
+        {
+            metrics
+                // Metrics provider from OpenTelemetry
+                .AddAspNetCoreInstrumentation()
+                .AddRuntimeInstrumentation()
+                .AddProcessInstrumentation()
+                .AddMeter(meterName)
+                // Metrics provides by ASP.NET Core in .NET 8
+                .AddMeter("Microsoft.AspNetCore.Hosting")
+                .AddMeter("Microsoft.AspNetCore.Server.Kestrel")
+                // Metrics provided by System.Net libraries
+                .AddMeter("System.Net.Http")
+                .AddMeter("System.Net.NameResolution")
+                .AddOtlpExporter(options =>
+                {
+                    options.Endpoint = new Uri(otlpEndpoint);
+                })
+                .AddPrometheusExporter();
+        });
+
+        return otel;
+    }
+
+    private static OpenTelemetryBuilder AddTracing(
+        this OpenTelemetryBuilder otel,
+        string applicationName,
+        string activityName,
+        string otlpEndpoint)
+    {
+        // Add Tracing for ASP.NET Core and our custom ActivitySource and export to Jaeger
+        otel.WithTracing(tracing =>
+        {
+            tracing.AddSource(applicationName)
+                .SetResourceBuilder(ResourceBuilder.CreateDefault()
+                    .AddService(applicationName));
+            tracing.AddAspNetCoreInstrumentation();
+            tracing.AddHttpClientInstrumentation();
+            tracing.AddSource(activityName);
+            if (otlpEndpoint != null)
+            {
+                tracing.AddOtlpExporter(otlpOptions =>
+                {
+                    otlpOptions.Endpoint = new Uri(otlpEndpoint);
+                });
+
+                tracing.AddJaegerExporter(); // Remove this for OTLP
+            }
+            else
+            {
+                tracing.AddConsoleExporter();
+            }
+        });
+
+        return otel;
     }
 }
